9ec3b249bfbbe1e140190951556099246aefa53f
ahmed <atyaf66@gmail.com>
2026-01-21 14:30:35 -0600

    chore: typo

--- packages/ims-client/src/index.ts
+++ packages/ims-client/src/index.ts
@@ -3,5 +3,5 @@
  */
 
 export { IMSClient } from './ims/default-ims-client';
-export { TokenIMSClient } from './ims/token-ims-clielt';
+export { TokenIMSClient } from './ims/token-ims-client';
 export type { IIMSClient, IMSClientOptions } from './ims/ims-client.interface';
